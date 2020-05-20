ac8477940e58c76a0d51ea2f279d573d9e09360a
cbini <cbini87@gmail.com>
2020-05-20 16:15:32 -0500

    join to new crosswalk table

--- q_nsc_sf_match.sql
+++ q_nsc_sf_match.sql
@@ -15,8 +15,25 @@ SELECT nsc.[unique]
       ,nsc.major
 
       ,r.salesforce_contact_id
+
+      ,a.id AS salesforce_school_id
 FROM ktx_analytics.dbo.nsc_hs_grad_college_data nsc
 LEFT JOIN ktx_analytics.dbo.sf_ktc_roster r
   ON nsc.[unique] = r.nsc_unique_identifier
--- JOIN nsc code to crosswalk
--- JOIN crosswalk to SF account
+LEFT JOIN ktx_analytics.dbo.nsc_college_code_crosswalk cw
+  ON nsc.[college code] = cw.nsc_college_code
+LEFT JOIN ktx_analytics.dbo.sf_account a
+  ON cw.ipeds_unit_id = a.ncesid__c
+ AND a.isdeleted = 0
+WHERE nsc.[Record Found] = 'Y'
+-- JOIN SF account to enrollments
+
+/*
+MATCH SF:
+ - graduated enrollments
+ - first enrollment
+
+Q?
+- would NSC_College_Data_Raw_HS_Grads better? (has college sequence)
+- which schools aren't matching crosswalk/SF and why?
+*/
