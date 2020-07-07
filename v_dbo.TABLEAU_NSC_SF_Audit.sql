728d13b28b5c1c63747ffbe82b1204ae14275024
cbini <cbini87@gmail.com>
2020-07-07 14:08:29 -0500

    add sex/ethnicity

--- v_dbo.TABLEAU_NSC_SF_Audit.sql
+++ v_dbo.TABLEAU_NSC_SF_Audit.sql
@@ -1,7 +1,32 @@
 CREATE OR ALTER VIEW dbo.TABLEAU_NSC_SF_Audit AS
 
-WITH nsc_link AS (
+WITH school_identifiers AS (
+  SELECT school_id
+        ,LEFT(region, 1) AS region_initial
+        ,CASE 
+          WHEN school = 'KIPP Northeast College Prep' THEN 'KIPP Northeast College Preparatory' 
+          WHEN school = 'KIPP Austin Brave' THEN 'KIPP Austin Brave High School'
+          WHEN school = 'KIPP Connect High School' THEN 'KIPP CONNECT Houston High School'
+          ELSE school 
+         END AS school
+  FROM ktx_analytics.dbo.school_names_table
+  WHERE academic_year = (SELECT MAX([year]) AS max_year FROM ktx_analytics.dbo.flat_region_year_dates)
+ )
+
+,demographics AS (
+  SELECT fkg.sex AS sex
+        ,fkg.[aggregate race ethnicity] AS ethnicity
+        ,fkg.[campus id]
+           + fkg.[student id]
+           + si.region_initial AS nsc_unique_identifier
+  FROM ktx_analytics.dbo.flat_kipp_graduates fkg
+  JOIN school_identifiers si
+    ON fkg.[campus id] = si.school_id
+ )
+
+,nsc AS (
   SELECT nsc.[unique] AS nsc_unique_identifier
+        ,nsc.[CO] AS nsc_cohort
         ,nsc.[first name] AS nsc_first_name
         ,nsc.[last name] AS nsc_last_name
         ,nsc.[college code] AS nsc_college_code
@@ -23,6 +48,9 @@ WITH nsc_link AS (
         ,r.counselor_name
         ,r.ktc_status
 
+        ,d.ethnicity
+        ,d.sex
+
         ,ROW_NUMBER() OVER(
            PARTITION BY nsc.[unique]
              ORDER BY nsc.[graduated], DATEFROMPARTS(RIGHT(nsc.[enrollment start], 4), LEFT(nsc.[enrollment start], 2), 1)) AS rn_enrollment_order
@@ -34,10 +62,12 @@ WITH nsc_link AS (
    AND a.isdeleted = 0
   LEFT JOIN ktx_analytics.dbo.sf_ktc_roster r
     ON nsc.[unique] = r.nsc_unique_identifier
+  LEFT JOIN demographics d
+    ON nsc.[unique] = d.nsc_unique_identifier
   WHERE nsc.[Record Found] = 'Y'
  )
 
-,sf_enr AS (
+,sf AS (
   SELECT enr.id AS salesforce_enrollment_id
         ,enr.[name] AS salesforce_enrollment_name
         ,enr.student__c AS salesforce_contact_id
@@ -59,8 +89,12 @@ WITH nsc_link AS (
         ,r.counselor_name
         ,r.kipp_region_name
         ,r.ktc_status
+        ,r.kipp_hs_class AS sf_cohort
 
         ,a.[name] AS sf_school_name
+
+        ,d.ethnicity
+        ,d.sex
 
         ,ROW_NUMBER() OVER(
            PARTITION BY enr.student__c
@@ -70,6 +104,8 @@ WITH nsc_link AS (
     ON enr.student__c = r.salesforce_contact_id
   JOIN ktx_analytics.dbo.sf_account a
     ON enr.school__c = a.id
+  LEFT JOIN demographics d
+    ON r.nsc_unique_identifier = d.nsc_unique_identifier
   WHERE enr.isdeleted = 0
     AND enr.type__c = 'College'
  )
@@ -81,13 +117,18 @@ SELECT n.nsc_unique_identifier
       ,n.counselor_name
       ,n.kipp_region_name
       ,n.nsc_college_name AS college_name
+      ,n.nsc_cohort AS cohort
+      ,n.ethnicity
+      ,n.sex
 
       ,n.salesforce_school_id AS nsc_school_id
+      ,n.nsc_college_name
       ,n.nsc_degree_title
       ,n.nsc_enrollment_start
       ,n.nsc_enrollment_end
 
       ,e.salesforce_school_id AS enr_school_id
+      ,e.sf_school_name
       ,e.sf_pursuing_degree_type
       ,e.sf_start_date
       ,e.sf_actual_end
@@ -96,28 +137,32 @@ SELECT n.nsc_unique_identifier
       ,CASE WHEN e.sf_school_name IS NOT NULL THEN 1 ELSE 0 END AS is_record_match
       ,'Graduated' AS enrollment_type
       ,'NSC>SF' AS direction
-FROM nsc_link n
-LEFT JOIN sf_enr e
+FROM nsc n
+LEFT JOIN sf e
   ON n.salesforce_contact_id = e.salesforce_contact_id
  AND n.salesforce_school_id = e.salesforce_school_id
+ AND e.sf_status = 'Graduated'
 WHERE n.nsc_graduated = 'Y'
   AND n.nsc_degree_title <> ''
-
 UNION ALL
-
 SELECT e.nsc_unique_identifier
       ,e.sf_first_name AS student_first_name
       ,e.sf_last_name AS student_last_name
       ,e.counselor_name
       ,e.kipp_region_name
       ,e.sf_school_name AS college_name
+      ,e.sf_cohort AS cohort
+      ,e.ethnicity
+      ,e.sex
 
       ,n.salesforce_school_id AS nsc_school_id
+      ,n.nsc_college_name
       ,n.nsc_degree_title
       ,n.nsc_enrollment_start
       ,n.nsc_enrollment_end
 
       ,e.salesforce_school_id AS enr_school_id
+      ,e.sf_school_name
       ,e.sf_pursuing_degree_type
       ,e.sf_start_date
       ,e.sf_actual_end
@@ -126,8 +171,8 @@ SELECT e.nsc_unique_identifier
       ,CASE WHEN n.nsc_college_name IS NOT NULL THEN 1 ELSE 0 END AS is_record_match
       ,'Graduated' AS enrollment_type
       ,'SF>NSC' AS direction
-FROM sf_enr e
-LEFT JOIN nsc_link n
+FROM sf e
+LEFT JOIN nsc n
   ON e.salesforce_contact_id = n.salesforce_contact_id
  AND e.salesforce_school_id = n.salesforce_school_id
  AND n.nsc_graduated = 'Y'
@@ -144,13 +189,18 @@ SELECT n.nsc_unique_identifier
       ,n.counselor_name
       ,n.kipp_region_name
       ,n.nsc_college_name AS college_name
-      
+      ,n.nsc_cohort AS cohort
+      ,n.ethnicity
+      ,n.sex
+
       ,n.salesforce_school_id AS nsc_school_id
+      ,n.nsc_college_name
       ,n.nsc_degree_title
       ,n.nsc_enrollment_start
       ,n.nsc_enrollment_end
 
       ,e.salesforce_school_id AS enr_school_id
+      ,e.sf_school_name
       ,e.sf_pursuing_degree_type
       ,e.sf_start_date
       ,e.sf_actual_end
@@ -159,28 +209,31 @@ SELECT n.nsc_unique_identifier
       ,CASE WHEN e.sf_school_name IS NOT NULL THEN 1 ELSE 0 END AS is_record_match
       ,'Matriculated' AS enrollment_type
       ,'NSC>SF' AS direction
-FROM nsc_link n
-LEFT JOIN sf_enr e
+FROM nsc n
+LEFT JOIN sf e
   ON n.salesforce_contact_id = e.salesforce_contact_id
  AND e.rn_enrollment_order = 1
 WHERE n.nsc_graduated = 'N'
   AND n.rn_enrollment_order = 1
-
 UNION ALL
-
 SELECT e.nsc_unique_identifier
       ,e.sf_first_name AS student_first_name
       ,e.sf_last_name AS student_last_name
       ,e.counselor_name
       ,e.kipp_region_name
       ,e.sf_school_name AS college_name
+      ,e.sf_cohort AS cohort
+      ,e.ethnicity
+      ,e.sex
 
       ,n.salesforce_school_id AS nsc_school_id
+      ,n.nsc_college_name
       ,n.nsc_degree_title
       ,n.nsc_enrollment_start
       ,n.nsc_enrollment_end
 
       ,e.salesforce_school_id AS enr_school_id
+      ,e.sf_school_name
       ,e.sf_pursuing_degree_type
       ,e.sf_start_date
       ,e.sf_actual_end
@@ -189,30 +242,10 @@ SELECT e.nsc_unique_identifier
       ,CASE WHEN n.nsc_college_name IS NOT NULL THEN 1 ELSE 0 END AS is_record_match
       ,'Matriculated' AS enrollment_type
       ,'SF>NSC' AS direction
-FROM sf_enr e
-LEFT JOIN nsc_link n
+FROM sf e
+LEFT JOIN nsc n
   ON e.salesforce_contact_id = n.salesforce_contact_id
  AND n.nsc_graduated = 'N'
  AND n.rn_enrollment_order = 1
 WHERE e.nsc_unique_identifier IS NOT NULL
   AND e.rn_enrollment_order = 1
-
-/*
-Questions:
-NEW
-- ugrad only?
-- why is there a discrepancy between number of students in SF vs NSC? who are they?
-OLDER
-- would [dbo].[NSC_College_Data_Raw_HS_Grads] better? (has college sequence)
-- which schools aren't matching crosswalk/SF and why?
-
-TODO
-- Dashboard:
-    * add KTX region/school
-    * add KTC counselor
-- match SF:
-    * matriculated enrollment
-- audit fields:
-    * start date (first enrollment)
-    * NSC query file (Flat_KIPP_Graduates) - does DOB match SalesForce?
-*/
